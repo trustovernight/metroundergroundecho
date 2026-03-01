54b2ca34aae338c83a39c99f2dda7fc2ed4bb041
trustovernight <vls445154@gmail.com>
2026-03-01 05:02:18 +0000

    Remove hunger

--- Assets/Scripts/Gameplay/Player/PlayerStats.cs
+++ Assets/Scripts/Gameplay/Player/PlayerStats.cs
@@ -12,18 +12,18 @@ namespace MetroUndergroundEcho.Gameplay
         public Slider staminaSlider;
 
         public float maxHealth = 100f;
-        public float maxHunger = 100f;
+        // public float maxHunger = 100f;
         public float maxStamina = 100f;
 
         [HideInInspector] public float health = 100f;
-        [HideInInspector] public float hunger = 100f;
+        // [HideInInspector] public float hunger = 100f;
         [HideInInspector] public float stamina = 100f;
 
         public Color emptyColor = Color.white;
         
         public float Delay = 0.5f;
         private float StaminaTimeRuning = 0f;
-        private float HungerTime = 0f;
+        // private float HungerTime = 0f;
 
         private Player player;
         private PauseManager pauseManager;
@@ -34,11 +34,11 @@ namespace MetroUndergroundEcho.Gameplay
             pauseManager = GameObject.Find("PauseObject").GetComponent<PauseManager>();
 
             healthSlider.maxValue = maxHealth;
-            hungerSlider.maxValue = maxHunger;
+            // hungerSlider.maxValue = maxHunger;
             staminaSlider.maxValue = maxStamina;
 
             healthSlider.value = maxHealth;
-            hungerSlider.value = maxHunger;
+            // hungerSlider.value = maxHunger;
             staminaSlider.value = maxStamina;
 
             StartCoroutine(WaitUpdate());
@@ -62,26 +62,26 @@ namespace MetroUndergroundEcho.Gameplay
                 player.isInAir = false;
             }
 
-            if (hunger <= 0f) 
-            {
-                stamina = 0f;
-                HungerTime += Time.deltaTime;
+            // if (hunger <= 0f) 
+            // {
+            //     stamina = 0f;
+            //     HungerTime += Time.deltaTime;
 
-                if (HungerTime >= Delay)
-                {
-                    ModifyHealth(-5f);
-                    HungerTime = 0f;
-                }
-            }
+            //     if (HungerTime >= Delay)
+            //     {
+            //         ModifyHealth(-5f);
+            //         HungerTime = 0f;
+            //     }
+            // }
         }
 
         private void FixedUpdate() {
-            if (hunger > 20f)
+            if (!player.IsRunning)
             {
                 if (stamina < 100f)
                 {
                     ModifyStamina(0.1f);
-                    ModifyHunger(-0.05f);
+                    // ModifyHunger(-0.05f);
                     Debug.Log($"{stamina} stamina");
                 }
             }
@@ -94,17 +94,17 @@ namespace MetroUndergroundEcho.Gameplay
                 if (!pauseManager.isPaused)
                 {
                     yield return new WaitForSeconds(1f);
-                    ModifyHunger(-0.5f);
-                    Debug.Log($"{hunger} hunger");
+                    // ModifyHunger(-0.5f);
+                    // Debug.Log($"{hunger} hunger");
                 }
             }
         }
 
-        public void ModifyHunger(float amount) 
-        {
-            hunger = Mathf.Clamp(hunger + amount, 0, maxHunger); 
-            hungerSlider.value = hunger;
-        }
+        // public void ModifyHunger(float amount) 
+        // {
+        //     hunger = Mathf.Clamp(hunger + amount, 0, maxHunger); 
+        //     hungerSlider.value = hunger;
+        // }
 
         public void ModifyStamina(float amount)
         {
